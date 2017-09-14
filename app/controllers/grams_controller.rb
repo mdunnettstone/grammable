8a70c7b2c36fa61861bc59e914d33f3c57a3347d
mdunnettstone <mdunnettstone@gmail.com>
2017-09-14 14:47:41 +0000

    Added users, tests and validation around new and create

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,24 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 end
